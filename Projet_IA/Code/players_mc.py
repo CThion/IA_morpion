72b4a49c1d84a02c92881f51939e1e76799bcd57
Clément Thion <clement.thion@orange.fr>
2022-03-14 23:24:48 +0100

    ajout de méthode auxiliaire __remember et __learn
    utilisation de méthode auxiliaire pour les requêtes sur self.memory (enregistrer nouvelles données et récupérer enregistrement)
     && ajout commentaires

--- Projet_IA/Code/players_mc.py
+++ Projet_IA/Code/players_mc.py
@@ -26,42 +26,83 @@ class NegAlphaBeta_Memory(Player): #optionnel 2
   """
   Implémentation similaires à celle de Negamax avec MinMax ;
   Regrouper coupe_alpha et coupe_beta en 1 seul coupe_alpha qui appelle -coupe_alpha
+  Paramètres rattaché à chaque config de memory : 
+    -- pf : la profondeur à laquelle l’état est rencontrée, c’est un entier,
+    -- exact : un booléen qui indique si l’évaluation est exacte ce qui est le cas lorsque la partie
+    est terminée, c’est-à-dire soit parce que on est sur une feuille avec self.game.over() est
+    True, soit parce que l’information a été remontée depuis un état où l’évaluation était exacte.
+    -- score : la valeur de l’évaluation
+    -- best_action : l’action ayant produit le score, lorsqu’il n’y a pas d’action, parce que la
+    partie est terminée, l’action sera None
   """
+
+
   def decision(self, state):
+    """Récupère l'état du jeu, et renvoit la meilleur décision possible
+    étant donnée la profondeur de calcul et les informations en mémoire
+    a_i = actions i dans self.game.actions
+    v_i = valeurs attribuée à  a_i"""
+    
+    #--vérificaiton légitimité du joueur
     self.game.state = state # on met à jour l’état du jeu
     if self.game.turn != self.who_am_i:
       print("not my turn to play")
       return None
-    # maintenant on peut travailler
-    pf = self.get_value('pf')
-    alpha = -101 
-    beta = 101  
-    liste_vi=[]
-    #--
+    
+    #--vérification mémoire
+    alpha = -101; beta = 101
+    pf = self.get_value('pf') #profondeur de calcul
+    action = self.__remember(pf, alpha, beta, 'best_action') #vérification mémoire
+    if action[2] !=None : return action 
+    
+    #--état totalement nouveau => calculs
+    liste_vi=[] #liste des valeurs estimées des actions les plus proches
+    #--parcours de toutes les actions les plus proches de la racines 
     for a_i in self.game.actions:   
-      #--changement d'état
-      self.game.move(a_i)
-      #--Etape 4
-      key = self.game.hash_code #Etape 4 - 1)
-      if key not in self.decision.memory : pass #Etape 4 - 2)
-      else: #Etape 4 _ 3)
-          if self.decision.memory[key]['exact']==True:
-              return self.game.actions[a_i]
-          elif pf <= self.decision.memory[key]['pf']:
-              return self.decision.memory[key]
-              
-      #--récupération informations
+      self.game.move(a_i) #changement d'état
       v_i = self.__coupe_alpha(pf-1, alpha, beta)   
       liste_vi.append(v_i)
       #--retour à l'état précédent
       self.game.undo()
-      #--traitement de l'information (sortie possible)
-    #--Cas de la nouvelle partie
-    maximum = liste_vi.index(max(liste_vi))
-    self.decision.memory.update({self.game.hash_code : {'pf':pf, "exact":True, "score":maximum, "best_action":self.game.actions[maximum]}}) #dico
-    return self.game.actions[maximum]
+    #--
+    maximum = liste_vi.index(max(liste_vi)) #indice de la meilleur action
+    best_action = self.game.actions[maximum] #meilleur action
+    self.__learn(pf, True, maximum, best_action)#enregistrement de ce nouvel état
+    return best_action
   # -----------------Partie Dictionnaire-----------
   decision.memory = {}
+  # ----------------------------
+  def __remember(self, pf, alpha, beta, wishinfo):
+    """Vérifie si l'état du jeu a déjà été traité dans self.memory, 
+    et si oui envoi le résultat correspondant en mémoire, 
+    ainsi que alpha et beta potentiellement mis à jour
+    wishinfo = 'best_action' ou 'score', change selon qu'on appelle __remember
+    depuis decision ou depuis cut"""
+
+    #--Etape 4 : utilisation de la mémoire 
+    res=None #résultat = None a priori
+    key = self.game.hash_code #état actuel du jeu
+    #--
+    if key in self.decision.memory : #si état déjà rencontré
+        #--si solution exacte (on sait comment gagner)
+        etat = self.decision.memory[key]
+        if etat[key]['exact'] or \
+        (not etat[key]['exact'] and pf<=etat[key]['pf']) : 
+            res=etat[wishinfo]
+        #--si la profondeur restante à explorer est supérieure à la profondeur de découverte de key
+        else:
+            alpha = max(alpha, etat[key]['score'])
+            if alpha >= beta: return beta # coupe
+    return alpha, beta, res
+  # ----------------------------
+  def __learn(self, pf, exact, score, best_action):
+      """Met a jour decision.memory"""
+      self.decision.memory.update(
+          {self.game.hash_code : {
+              'pf':pf, 
+              "exact":True, 
+              "score":score, 
+              "best_action":best_action}})
   # -----------------------------------------------
   def __coupe_alpha(self, pf, alpha, beta):
     """MIN cherche a diminuer beta"""
